4bec6e9216bf160db3dc6e3f764324063586003e
Zaywreck <mgulle35@gmail.com>
2022-11-01 15:57:05 +0300

    :)
    :)

---
+++ forum/src/data/data.js
@@ -0,0 +1,9 @@
+const employees = [
+    {id: 1, firstName:"Mert", lastName:"Gülle", age:15}, 
+    {id: 2, firstName:"Refet", lastName: "Bele", age:28},
+    {id: 3, firstName:"Fevzi", lastName: "Çakmak", age:32},
+    {id: 4, firstName:"Mustafa Kemal", lastName: "Atatürk", age:48},
+    {id: 5, firstName:"İsmet", lastName: "İnönü", age:25}
+  ];
+
+export {employees};
